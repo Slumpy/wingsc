9a0c60f81f1e90e278e54c330ece22a38a929b87
Woody Gilk <woody.gilk@kohanaphp.com>
2009-10-10 19:03:30 -0500

    Slight changes to content

--- application/views/contact/index.php
+++ application/views/contact/index.php
@@ -2,10 +2,9 @@
 
 	<h1 class="intro">If you want more information about the services that <em>w.ings consulting</em> can provide for your business, or if you have comments about this website, please <?php echo html::mailto('woody@wingsc.com', 'email me') ?>. If you are considering hiring me for a project, please use the contact form below.</h1>
 
-	<div class="span-12 prefix-5 suffix-5 last">
+	<div class="span-14 suffix-4 colborder">
 		<h2>Send Me A Lead</h2>
-		<p class="byline">My response time is based entirely on how busy I am with current projects.<br/>
-			It may take up to a week for me to respond to your message.</p>
+		<p class="byline">Please allow a day or two for me to review your information and send a response.</p>
 
 		<?php echo $content ?>
 	</div>
--- application/views/pages/is.php
+++ application/views/pages/is.php
@@ -2,6 +2,6 @@
 
 	<h1 class="intro"><em>w.ings consulting</em> is the web development business of <em>Woody Gilk</em>. I design, implement, and optimize digital solutions to effectively communicate information from one person to another.</h1>
 
-	<p>I offer <abbr title="PHP Hypertext Preprocessor">PHP</abbr> and <abbr title="Asynchronous JavaScript And XML">AJAX</abbr> programming, <abbr title="eXtensible HyperText Markup Language">XHTML</abbr> and <abbr title="Cascading Style Sheet">CSS</abbr> scripting, as well as <?php echo html::anchor('http://kohanaphp.com/', 'KohanaPHP') ?> support, security consulting, database design, search engine optimization and code review. A <?php echo html::anchor(Route::get('default')->uri(array('page' => 'does')), 'complete list of services') ?> is available.</p>
+	<p>More specifically, I <?php echo html::anchor(Route::get('default')->uri(array('page' => 'does')), 'build and design websites') ?> and web applications. Almost all of my work <?php echo html::anchor(Route::get('work')->uri(), 'is done') ?> using some of the best open source programming tools available. Among these tools are the <?php echo html::anchor('http://kohanaphp.com/', 'Kohana PHP framework') ?>, <?php echo html::anchor('http://www.mysql.com/', 'MySQL database') ?>, <?php echo html::anchor('http://jquery.com/', 'jQuery Javascript') ?>, and <?php echo html::anchor('http://bluetrip.org/', 'BlueTrip CSS framework') ?>. Of course, no tool is perfect for every project. I believe there is always a solution for communicating effectively with your audience.</p>
 
 </div>
